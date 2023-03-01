a77c2b8309d8593394468ab57bdc02e242713a28
wjy <wjy@qq.com>
2023-03-01 20:08:05 +0800

    5555

--- src/main/java/com/wjy/GitTest.java
+++ src/main/java/com/wjy/GitTest.java
@@ -3,6 +3,6 @@ package com.wjy;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
-        Systemo.out.pringln("hhhhhhhhh");
+        System.out.println("zxccccccxczxczczx");
     }
 }
