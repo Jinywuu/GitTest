656d4b55311976a5ca63cae8da889e5631aeba1c
wjy <wjy@qq.com>
2023-03-01 16:49:47 +0800

    Second commit

--- src/main/java/com/wjy/GitTest.java
+++ src/main/java/com/wjy/GitTest.java
@@ -3,5 +3,7 @@ package com.wjy;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git2");
+        System.out.println("hello Git3");
     }
 }
