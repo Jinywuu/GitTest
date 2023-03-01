0807b0ce20cb0280ac5980bbb91c57c25d2e5722
wjy <wjy@qq.com>
2023-03-01 19:59:48 +0800

    the new commit

--- src/main/java/com/wjy/GitTest.java
+++ src/main/java/com/wjy/GitTest.java
@@ -3,10 +3,5 @@ package com.wjy;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
-        System.out.println("hello Git2");
-        System.out.println("hello hot-fix");
-        System.out.println("hello hot-fix");
-        System.out.println("hot-fix test");
-        System.out.println("push      test");
     }
 }
