a953be0c6eecb47603dfa87baabfd441b1eff050
wjy <wjy@qq.com>
2023-03-01 20:26:10 +0800

    向github发布的项目

--- src/main/java/com/wjy/GitTest.java
+++ src/main/java/com/wjy/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("zxccccccxczxczczx");
+        System.out.println("向github发布项目");
     }
 }
