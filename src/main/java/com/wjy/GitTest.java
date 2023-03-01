cd34973c8114b02f69a25bb407cc6f49675e9429
wjy <wjy@qq.com>
2023-03-01 20:40:09 +0800

    新版本完成

--- src/main/java/com/wjy/GitTest.java
+++ src/main/java/com/wjy/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("向github   push项目");
         System.out.println("这是hotfix的修改，请主分支合并");
         //我收到了你的push  这是新的版本 请pull回去
+        System.out.println("新的版本维护完成！！！");
     }
 }
